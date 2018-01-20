72e52b9514db2f5b3c202853735b4fc136957440
awong43 <awong43@ucsc.edu>
2018-01-20 03:00:27 -0800

    idk edit

---
+++ app/src/main/java/com/cruzhacks/scheduleapp/Assignment.java
@@ -0,0 +1,24 @@
+package com.cruzhacks.scheduleapp;
+import java.util.LinkedList;
+/**
+ * Created by cante on 1/20/2018.
+ */
+
+
+
+public class Assignment {
+    public String names;
+    public int months;
+    public int days;
+    public int years;
+
+    public Assignment (String name, int month, int day, int year) {
+        this.names = name;
+        this.months = month;
+        this.days = day;
+        this.years = year;
+
+    }
+}
+
+
