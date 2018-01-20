bbbd5c5b78ffaa54865f621614399c3512e2b412
awong43 <awong43@ucsc.edu>
2018-01-20 04:37:07 -0800

    finished classes?

--- app/src/main/java/com/cruzhacks/scheduleapp/Classes.java
+++ app/src/main/java/com/cruzhacks/scheduleapp/Classes.java
@@ -6,21 +6,28 @@ import java.util.LinkedList;
  * Created by cante on 1/20/2018.
  */
 
-public class Classes {
-    public LinkedList<Assignment> assignments;
-    public int colorAssigns;
-    public String classNames;
-    public String days;
-    public int startTime;
-    public int endTime;
 
-    public Classes(Assignment assignment, int color, String name, String day, int start, int end) {
+public class Classes {
+    public LinkedList<Assignment> assignments;  //LinkedList of assignments for a class
+    public int colorAssigns;                    //colored label for each class
+    public String classNames;                   //name of class
+    public String days;                         //days held per week
+    public int startTimeHour;                   //hour of start time
+    public int startTimeMin;                    //minute of start time
+    public int endTimeHour;                     //hour of end time
+    public int endTimeMin;                      //minute of end time
+
+    public Classes(Assignment assignment, int color, String name, String day, int startHour, int startMin, int endHour, int endMin) {
         this.assignments.add(assignment);
         this.colorAssigns = color;
         this.classNames = name;
         this.days = day;
-        this.startTime = start;
-        this.endTime = end;
+        this.startTimeHour = startHour;
+        this.startTimeMin = startMin;
+        this.endTimeHour = endHour;
+        this.endTimeMin = endMin;
     }
+
+
 
 }
